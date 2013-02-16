4e71119aff9c6677b6d8162860dcda6b0e421ccd
Jonathan <jrheavin@gmail.com>
2013-02-15 21:25:05 -0600

    Updated Serialization
    Fixed Serialization to work with more complicated classes

--- CapstoneProject/TileEngine/Map.cs
+++ CapstoneProject/TileEngine/Map.cs
@@ -9,11 +9,12 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
+using Polenter.Serialization;
 
 namespace TileEngine
 {
     /// <summary>
-    /// A Map is the basic structure for storing the tile engine data.  
+    /// A Map is the basic structure for storing the tile engine data.
     /// That includes tileSheets, Tiles, layers, npcs and anything else the player interacts with.
     /// Does practically all the heavy lifting.
     /// </summary>
@@ -22,6 +23,8 @@ namespace TileEngine
         //For now I'm hard coding the layers eventually we want to move to a list
         //based system.
         DrawableLayer<Tile> _Ground;
+
+        CustomSerialization.Serialize<Map> serializer = new CustomSerialization.Serialize<Map>();
 
         public DrawableLayer<Tile> Ground
         {
@@ -36,6 +39,7 @@ namespace TileEngine
             get { return _Mask; }
             set { _Mask = value; }
         }
+
         DrawableLayer<Tile> _Fringe;
 
         public DrawableLayer<Tile> Fringe
@@ -46,7 +50,7 @@ namespace TileEngine
 
         //Layers for collision detection
         Layer<Rectangle> _CollisionLayer;
-        
+
         //Abstract Type for Player
         Avatar _Player;
 
@@ -59,16 +63,16 @@ namespace TileEngine
         /// <summary>
         /// Default Doesn't do anything.
         /// </summary>
-        public Map(){}
-        
+        public Map() { }
+
         //TODO:  Probably need to add a constuctor the gets the graphics device and does its "magic"
         public Map(DrawableLayer<Tile> Ground, DrawableLayer<Tile> Mask, DrawableLayer<Tile> Fringe)
         {
             _Ground = Ground;
             _Mask = Mask;
             _Fringe = Fringe;
-        
         }
+
         /// <summary>
         /// Updates all map specific components
         /// </summary>
@@ -76,57 +80,54 @@ namespace TileEngine
         public void Update(GameTime gameTime)
         {
             _Player.Update(gameTime);
-        
-        
         }
 
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
             //TODO:  Add a way to calculate the where the center should be based on the player.
 
-            
             _Ground.Draw(spriteBatch, gameTime, _Player.Position);
             _Mask.Draw(spriteBatch, gameTime, _Player.Position);
             _Player.Draw(spriteBatch, gameTime);
             _Fringe.Draw(spriteBatch, gameTime, _Player.Position);
-                  
         }
-    
+
         /// <summary>
         /// Swaps out the Ground Layer
         /// </summary>
         /// <param name="swapLayer">The layer to be added to the Map</param>
-       public void SwapGoundLayer(DrawableLayer<Tile> swapLayer)
+        public void SwapGoundLayer(DrawableLayer<Tile> swapLayer)
         {
             if (_Ground == null)
                 _Ground = swapLayer;
             else
-           _Ground.swapLayer(swapLayer); 
-        
-       }
+                _Ground.swapLayer(swapLayer);
+        }
 
-       public void SwapMaskLayer(DrawableLayer<Tile> swapLayer)
-       {
-           if (_Mask == null)
-               _Mask = swapLayer;
-           else
-           _Mask.swapLayer(swapLayer);
+        public void SwapMaskLayer(DrawableLayer<Tile> swapLayer)
+        {
+            if (_Mask == null)
+                _Mask = swapLayer;
+            else
+                _Mask.swapLayer(swapLayer);
+        }
 
-       }
+        public void SwapFringeLayer(DrawableLayer<Tile> swapLayer)
+        {
+            if (_Fringe == null)
+                _Fringe = swapLayer;
+            else
+                _Fringe.swapLayer(swapLayer);
+        }
 
-       public void SwapFringeLayer(DrawableLayer<Tile> swapLayer)
-       {
-           if (_Fringe == null)
-               _Fringe = swapLayer;
-           else
-           _Fringe.swapLayer(swapLayer);
+        public void saveMap(/*string filename*/)
+        {
+            serializer.Save(this, true);
+        }
 
-       }
-
-       public void saveMap(/*string filename*/)
-       {
-           CustomSerialization.Serialize<Map> serializer = new CustomSerialization.Serialize<Map>();
-           serializer.Save(this);
-       }
+        public void LoadMap(string filename)
+        {
+            serializer.Load("SaveGame.xml");
+        }
     }
 }
