d3c6b02c2b1c223dbb317eb280490745103fcd80
Tim Britton <tbritton03@aurora.edu>
2013-04-05 00:26:20 -0500

    More MapEditor Stuff
    Changed the map class to allow rendering to a specific area of the
    screen through mapWindows.

--- CapstoneProject/MapEditor/MapEditor/Game1.cs
+++ CapstoneProject/MapEditor/MapEditor/Game1.cs
@@ -10,6 +10,7 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 using TileEngine;
 using CustomSerialization;
+using MapEditor.GUI;
 
 namespace MapEditor
 {
@@ -20,25 +21,14 @@ namespace MapEditor
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        Map _newMap = new Map();
-
-        DrawableLayer<Tile> _ground;
-        DrawableLayer<Tile> _mask;
-        DrawableLayer<Tile> _Fringe;
-
-        FocalPoint _center = new FocalPoint(new Vector2(10, 10), 10, 10, 100, 60);
-
-        enum currentLayer { GROUND, MASK, FRINGE}
-        /// <summary>
-        /// For determining what layer you are laying tiles in.
-        /// </summary>
-        currentLayer _currentLayer = currentLayer.GROUND;
+        MapWindow window;
+   
         public MapEditorMain()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
-
+        
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
@@ -48,9 +38,7 @@ namespace MapEditor
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            _ground = new DrawableLayer<Tile>();
-            _mask = new DrawableLayer<Tile>();
-            _Fringe = new DrawableLayer<Tile>();
+           
             base.Initialize();
         }
 
@@ -62,7 +50,7 @@ namespace MapEditor
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            window = new MapWindow(spriteBatch, graphics, Content);
             // TODO: use this.Content to load your game content here
         }
 
@@ -82,13 +70,12 @@ namespace MapEditor
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            _center.Update(gameTime);
+            
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
-            _newMap = new Map(_ground, _mask, _Fringe);
-            _newMap.Player = _center;
-            
+
+            window.Update(gameTime);
 
             base.Update(gameTime);
         }
@@ -102,6 +89,7 @@ namespace MapEditor
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            window.Draw(gameTime);
 
             base.Draw(gameTime);
         }
